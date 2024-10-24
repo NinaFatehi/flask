a175a36328616bfd90216df9e7c769eb21a0f1bc
NinaFatehi <117478988+NinaFatehi@users.noreply.github.com>
2024-10-24 15:52:15 +0000

    Added few comments to app.py

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ def random_fruit():
     fruits = ["apple", "cherry", "orange", "banana"]
     return choices(fruits)
 
-
+## A homepage of a web application and returns back the random fruit from the radom_fruit() function!
 @app.route("/")
 def fruit():
     """Return random fruit"""
 
     my_fruit = random_fruit()
-    return render_template("index.html", title="Random Fruit", fruit=my_fruit)
+    return render_template("index.html", title="Random Fruit", fruit=my_fruit)  ## a template to do a little bit of html
 
 
 if __name__ == "__main__":
